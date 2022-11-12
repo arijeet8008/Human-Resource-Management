4950dd5d00bef264cb22e3afccec0d53da97f4aa
arijeet@8008 <arijeetchowdhury8008@gmail.com>
2022-11-12 21:08:45 +0530

    m1.1

--- Human Resource Management System/src/com/hrms/usecases/TransferEmployeeDepartmnet.java
+++ Human Resource Management System/src/com/hrms/usecases/TransferEmployeeDepartmnet.java
@@ -7,28 +7,28 @@ import com.hrms.dao.EmployeeDaoImpl;
 import com.hrms.exceptions.EmployeeException;
 
 public class TransferEmployeeDepartmnet {
-/*
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter Employee ID :");
-		int edid = sc.nextInt();
-		
-		System.out.println("Enter Employee Department ID :");
-		int empdid = sc.nextInt();
-		
-		EmployeeDao dao = new EmployeeDaoImpl();
-		
-		try {
-			String result = dao.transferEmployee(edid, empdid);
-			System.out.println(result);
-		} catch (EmployeeException e) {
-			System.out.println(e.getMessage());
-		}
-		
-	}
-	*/
+
+//	public static void main(String[] args) {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Enter Employee ID :");
+//		int edid = sc.nextInt();
+//		
+//		System.out.println("Enter Employee Department ID :");
+//		int empdid = sc.nextInt();
+//		
+//		EmployeeDao dao = new EmployeeDaoImpl();
+//		
+//		try {
+//			String result = dao.transferEmployee(edid, empdid);
+//			System.out.println(result);
+//		} catch (EmployeeException e) {
+//			System.out.println(e.getMessage());
+//		}
+//		
+//	}
+	
 	
 	public static void transferEmployee() {
 		
